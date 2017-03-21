eb27f5dfc457862563211a11c8eef357ef852206
kkdd <kkondo_z0@nifty.com>
2017-03-21 21:59:47 +0900

    Update UpdateParameterAndConditionBulder.kt

--- src/main/kotlin/net/devneko/kjdbc/UpdateParameterAndConditionBulder.kt
+++ src/main/kotlin/net/devneko/kjdbc/UpdateParameterAndConditionBulder.kt
@@ -2,11 +2,45 @@ package net.devneko.kjdbc
 
 class UpdateParameterAndConditionBulder() : UpdateParameterBuilder() {
 
-    private var _cond:String = "";
+    private var _cond:String = ""
     public val condition:String get() = _cond
 
-    fun where(cond:String, block: ParameterMapper.()->Unit):Unit {
-        this._cond = cond;
-        _setList.add(block)
+    private var _select:String = ""
+    public val selection:String get() = _select
+
+    private var _limit:String = ""
+    public val limit:String get() = _limit
+
+    private var _orderBy:String = ""
+    public val orderBy:String get() = _orderBy
+
+    protected val _columnList = arrayListOf<(ColumnNameMapper)->Unit>()
+
+    fun selectionBlock(): ColumnNameMapper.()->Unit {
+        return {
+           _columnList.forEach { it(this) }
+        }
+    }
+
+    fun select(select:String, block: (ColumnNameMapper.()->Unit)? = null):Unit {
+        this._select = select
+        block?.let {
+            _columnList.add(block)
+        }
+    }
+
+    fun where(cond:String, block: (ParameterMapper.()->Unit)? = null ):Unit {
+        this._cond = cond
+        block?.let {
+            _setList.add(block)
+        }
+    }
+
+    fun limit(limit:Int):Unit {
+        this._limit = limit.toString()
+    }
+
+    fun orderBy(name:String):Unit {
+        this._orderBy = name
     }
 }
